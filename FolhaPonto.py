944a31a827ec24b213bdc5f3e6e2803ea00a9eac
Joao Pedro Soares <jpos99@hotmail.com>
2022-06-22 17:01:16 -0300

    deploy18

--- FolhaPonto.py
+++ FolhaPonto.py
@@ -2,5 +2,7 @@ from app import app
 import os
 
 port = int(os.environ.get("PORT", 15000))
-app.run(port=port)
+
+if __name__ == "__main__":
+        app.run(port=port)
 #app.run(host='127.0.0.1', port=17000)
